4ca4d9e0eb22e3b85a3a88b72ad1f17b7122e507
yashaghane21 <yashaghane21@gmail.com>
2025-12-02 14:15:12 +0530

    ff

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -19,7 +19,7 @@ export default function Success() {
           { withCredentials: true }
         );
 
-        setStatus("✅ Payment successful!");
+        setStatus("✅ Payment succcessful!");
 
         window.ReactNativeWebView?.postMessage(
           JSON.stringify({ type: "payment-status", status: "success" })
